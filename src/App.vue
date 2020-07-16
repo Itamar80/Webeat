3f1165ea55ff4f13687808fd315681655936590e
Itamar80 <65674792+Itamar80@users.noreply.github.com>
2020-07-16 09:25:04 +0300

    adding sass

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div class="main-nav flex space-between align-center justify-center container">
       <span>logo</span>
+      <div>
       <router-link to="/">Home</router-link> |
       <!-- <router-link to="/dashboard">Statistics</router-link> | -->
       <router-link to="/about">About</router-link>
+      </div>
     </div>
     <router-view/>
   </div>
