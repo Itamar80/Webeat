0929924de73a5017ff879b153fc7809ff0f7788b
Itamar80 <65674792+Itamar80@users.noreply.github.com>
2020-07-16 09:30:04 +0300

    sass

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <div class="main-nav flex space-between align-center justify-center container">
       <span>logo</span>
       <div>
-      <router-link to="/">Home</router-link> |
-      <!-- <router-link to="/dashboard">Statistics</router-link> | -->
-      <router-link to="/about">About</router-link>
+      <router-link class="router-link" to="/">Home</router-link> |
+      <router-link class="router-link" to="/dashboard">Statistics</router-link> |
+      <router-link class="router-link" to="/about">About</router-link>
       </div>
     </div>
     <router-view/>
