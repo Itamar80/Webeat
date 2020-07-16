bccb141cc34fba22f0ff48dfa0efb17e58e0f674
Nir <d.nir83@gmail.com>
2020-07-16 16:29:16 +0300

    changed a little at homepage

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
 <header class="header">
     <div class="main-header flex space-between align-center justify-center container">
       <span class="logo">logo</span>
-      <div>
-      <router-link class="router-link" to="/">Home</router-link> |
-      <router-link class="router-link" to="/dashboard">Statistics</router-link> |
-      <router-link class="router-link" to="/about">About</router-link> |
-      <router-link class="router-link" to="/login">Login</router-link>
-      </div>
+      <nav>
+      <router-link class="nav-btn router-link" to="/">Home</router-link> |
+      <router-link class="nav-btn router-link" to="/dashboard">Statistics</router-link> |
+      <router-link class="nav-btn router-link" to="/about">About</router-link> |
+      <router-link class="nav-btn router-link" to="/login">Login</router-link>
+      </nav>
     </div>
 </header>
     <router-view/>
