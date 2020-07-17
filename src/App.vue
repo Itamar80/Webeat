5f41ac3e6347c2c6d54720deaa066ce6ad5fec6b
Naomi <naomi.elka@gmail.com>
2020-07-17 22:06:01 +0300

    homepage and nav design improvements

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@
     <div class="main-header flex space-between align-center justify-center container">
       <span class="logo">logo</span>
       <nav>
-      <router-link class="nav-btn router-link" to="/">Home</router-link> |
-      <router-link class="nav-btn router-link" to="/dashboard">Statistics</router-link> |
-      <router-link class="nav-btn router-link" to="/about">About</router-link> |
+      <router-link class="nav-btn router-link" to="/">Home</router-link> 
+      <router-link class="nav-btn router-link" to="/stations">Stations</router-link> 
+      <router-link class="nav-btn router-link" to="/dashboard">Statistics</router-link> 
+      <router-link class="nav-btn router-link" to="/about">About</router-link> 
       <router-link class="nav-btn router-link" to="/login">Login</router-link>
       </nav>
     </div>
@@ -14,5 +15,6 @@
     <router-view/>
   </div>
 </template>
+
 
 
--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,14 +1,54 @@
 <template>
   <div class="homepage">
-    <img class="hero-img" src="../assets/people.webp" alt="">
-      <router-link class="station-link router-link" to="/stations">Get Started</router-link> 
+    <!-- <img class="hero-img" :src="img" alt=""> -->
+    <div class="hero-img">
+      <img src="@/assets/hero.jpg" />
+      <img src="@/assets/hero1.webp" />
+      <img src="@/assets/hero 2.webp" />
+      <img src="@/assets/hero3.webp" />
+    </div>
+    <div class="container">
+        <router-link :to="'/stations'" class="button">Get Started</router-link>
+    </div>
+    <!-- <div class="wrap">
+      <button class="button" @click="start">Get Started</button> 
+    </div> -->
   </div>
 </template>
 
 <script>
+// import hero from "@/assets/hero.jpg"
+// import hero1 from "@/assets/hero1.webp"
+// import hero2 from "@/assets/hero 2.webp"
+// import hero3 from "@/assets/hero3.webp"
 export default {
   name: 'homePage',
-  components: {
-  }
+  data(){
+    return{
+      // img: hero3
+    }
+  },
+  created(){
+    // this.getImg()
+  },
+  methods:{
+    start(){
+      this.$router.push('/stations')
+    }
+  },
+  //   getImg(){
+  //   var i = 0;
+  //   var imgUrls = [hero, hero1,hero2,hero3];
+  //   const intervalId = setInterval(()=> {
+  //     if (i > imgUrls.length-1) i=0;
+  //     this.img = imgUrls[i] 
+  //     i++
+  //     }, 5000)
+  //   }
+    
+  // }, destroyed(){
+  //   clearInterval(intervalId)
+  // },
+  components: {}
 }
 </script>
