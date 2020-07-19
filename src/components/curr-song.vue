004fc5e2854d164acee9943a61a6157b80d3b6e9
Naomi <naomi.elka@gmail.com>
2020-07-19 17:38:20 +0300

    station edit progress

--- src/components/curr-song.vue
+++ src/components/curr-song.vue
@@ -1,13 +1,34 @@
 <template>
   <section class="curr-song">
-    <div class="iframe-container">
-      <youtube :video-id="videoId" :player-vars="playerVars"  @ended="ended" ref="youtube"></youtube>
+    <div class="flex space-around">
+      <h1>{{station.name}}</h1>
+      <div class="iframe-container">
+        <youtube
+          :video-id="videoId"
+          :player-vars="playerVars"
+          @playing="playing"
+          @ended="ended"
+          ref="youtube"
+        ></youtube>
+      </div>
     </div>
     <section class="song-controllers flex row justify-center align-center">
-      <font-awesome-icon @click="changeSong('prevSong')" icon="backward" size="lg" class="control-icon" />
+      <!-- <input id="progressBar" type="range" /> -->
+      <span>{{ time }}/{{ duration }}</span>
+      <font-awesome-icon
+        @click="changeSong('prevSong')"
+        icon="backward"
+        size="lg"
+        class="control-icon"
+      />
       <font-awesome-icon @click="playVideo" icon="play" size="lg" class="control-icon" />
       <font-awesome-icon @click="pauseVideo" icon="pause" size="lg" class="control-icon" />
-      <font-awesome-icon @click="changeSong('nextSong')" icon="forward" size="lg" class="control-icon" />
+      <font-awesome-icon
+        @click="changeSong('nextSong')"
+        icon="forward"
+        size="lg"
+        class="control-icon"
+      />
     </section>
   </section>
 </template>
@@ -28,31 +49,31 @@ library.add(faBackward);
 export default {
   name: "curr-song",
   props: {
+    station: Object,
     currSong: Object
   },
   data() {
     return {
-       playerVars: {
-        autoplay: 1,
+      playerVars: {
+        // autoplay: 1,
         // controls: 0,
-        modestbranding:1,
+        modestbranding: 1,
         showinfo: 0
-      }
+      },
+      time: "00:00",
+      duration: "00:00",
+      timeId: null
     };
   },
-  mounted() {
-  },
+  mounted() {},
   computed: {
     videoId() {
       return this.currSong.youtubeId;
     },
     player() {
+      console.log(this.$refs.youtube.player);
       return this.$refs.youtube.player;
     }
-    // videoSrc() {
-    //   // return `//www.youtube.com/embed/${this.currSong.youtubeId}?enablejsapi=1`
-    //   return `//www.youtube.com/embed/${this.currSong.youtubeId}?enablejsapi=1;&autoplay=1`;
-    // },
   },
   methods: {
     async playVideo() {
@@ -61,19 +82,49 @@ export default {
     async pauseVideo() {
       await this.player.pauseVideo();
     },
-    changeSong(type){
-      this.$emit('changeSong', type, this.currSong)
+    changeSong(type) {
+      this.$emit("changeSong", type, this.currSong);
     },
-    ended(){
-      this.changeSong('nextSong')
+    ended() {
+      this.changeSong("nextSong");
+    },
+      async showDuration() {
+        return await this.player.getDuration();
+      },
+
+      playing() {
+        this.timeId = setInterval(() => {
+          this.player.getCurrentTime().then(time => {
+            this.time = this.formatTime(time + 1);
+          });
+        }, 100);
+      },
+      formatTime(time) {
+        time = Math.round(time);
+        let minutes = Math.floor(time / 60);
+        let seconds = time - minutes * 60;
+
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+
+        return minutes + ":" + seconds;
+      },
+      ended() {
+        this.time = "00:00";
+        clearInterval(this.timeId);
+      }
+    },
+    async mounted() {
+      this.duration = this.formatTime(await this.player.getDuration());
+    },
+    components: {
+      fontAwsomeIcon
     }
-   
-  },
-  components: {
-    fontAwsomeIcon
-  }
+  
 };
 </script>
 
 <style>
 </style>
+
+     
