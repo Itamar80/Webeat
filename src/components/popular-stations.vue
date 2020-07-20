596f5b1d79af142fe88f3c090d2e73d745d9e134
Itamar80 <65674792+Itamar80@users.noreply.github.com>
2020-07-20 19:49:23 +0300

    fixing css

--- src/components/popular-stations.vue
+++ src/components/popular-stations.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="popular-station-list container">
-    <ul class="clean-list flex justify-center">
+    <ul class="clean-list flex space-between">
       <li v-for="station in popularStations" :key="station._id">
         <station-preview :station="station" @toggleLike="toggleLike"></station-preview>
       </li>
--- src/views/genre-stations.vue
+++ src/views/genre-stations.vue
@@ -1,56 +1,20 @@
 <template>
   <div class="stations-page">
     <div class="container">
-    <station-filter class="filter" @filterStations="setFilter"/>
     </div>
-      <section class="genre-stations">
+     <section class="genre-stations"> 
     <h1 v-if="genre" class="h1 container yellow">Genre: {{genre.charAt(0).toUpperCase()+ genre.slice(1)}}</h1>
     <h1 v-else class="h1 container yellow"> Stations</h1>
-     <station-list class="container" v-if="genre" :genre="genre" :stations="stations" @toggleLike="toggleLike"></station-list>
-     <section class="all-stations" v-else>
+    <station-filter class="filter flex justify-center" @filterStations="setFilter"/>
+     <station-list class="container" v-if="genre"  :genre="genre" :stations="stations" @toggleLike="toggleLike"></station-list>
+     <section class="all-stations" v-else  v-for="singleGenre in genres" :key="singleGenre">
     <div class="container flex space-between">
-    <h3 class="">Hip-Hop</h3>
-     <span @click="moveTo('hiphop')">Show All</span>
+    <h3 class="">{{singleGenre.charAt(0).toUpperCase()+singleGenre.slice(1)}}</h3>
+     <span class="show-all" @click="moveTo(singleGenre)">Show All</span>
     </div>
     <hr/>
-     <station-list class="container" :genre="'hiphop'" :stations="stations" @toggleLike="toggleLike"></station-list>
-     <div class="container flex space-between">
-    <h3 class="">Arabic</h3>
-    <span @click="moveTo('arabic')">Show All</span>
-    </div>
-    <hr/>
-     <station-list class="container" :genre="'arabic'"  :stations="stations" @toggleLike="toggleLike"></station-list>
-    <div class="container flex space-between">
-    <h3 class="">Rock</h3>
-     <span @click="moveTo('rock')">Show All</span>
-    </div>
-    <hr/>
-     <station-list class="container" :genre="'rock'"  :stations="stations" @toggleLike="toggleLike"></station-list>
-    <div class="container flex space-between">
-    <h3 class="">Easy</h3>
-     <span @click="moveTo('easy')">Show All</span>
-    </div>
-     <station-list class="container" :genre="'easy'"  :stations="stations" @toggleLike="toggleLike"></station-list>
-    <hr/>
-    <div class="container flex space-between">
-    <h3 class="">Electronic</h3>
-     <span @click="moveTo('electronic')">Show All</span>
-    </div>
-    <hr/>
-     <station-list class="container" :genre="'electronic'"  :stations="stations" @toggleLike="toggleLike"></station-list>
-    <div class="container flex space-between">
-    <h3 class="">Pop</h3>
-     <span @click="moveTo('pop')">Show All</span>
-    </div>
-    <hr/>
-     <station-list class="container" :genre="'pop'"  :stations="stations" @toggleLike="toggleLike"></station-list>
-    <div class="container flex space-between">
-    <h3 class="">Jazz</h3>
-     <span @click="moveTo('jazz')">Show All</span>
-    </div>
-    <hr/>
-     <station-list class="container" :genre="'jazz'"  :stations="stations" @toggleLike="toggleLike"></station-list>
-     </section>
+   <station-list class="container" :genre="singleGenre" :stations="stations" @toggleLike="toggleLike"></station-list>
+     </section> 
     </section>
   </div>
 </template>
@@ -65,7 +29,8 @@ export default {
   data(){
     return {
       stations: [],
-      genre:''
+      genres:['hiphop','arabic','easy','electronic','country','flamenco','jazz','rock','pop'],
+      genre:'',
     }
   },
   created(){
